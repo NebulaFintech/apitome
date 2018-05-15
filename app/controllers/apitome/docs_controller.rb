4ee2190b85efbfad0da1d71e07cb32451dbc5eca
Mauricio Murga <murga.mauricio@gmail.com>
2018-05-15 16:47:09 -0500

    fix typo in docs controller

--- app/controllers/apitome/docs_controller.rb
+++ app/controllers/apitome/docs_controller.rb
@@ -1,6 +1,6 @@
 require 'open-uri'
 
-class Apitome::DocsController < < Object.const_get(Apitome.configuration.parent_controller)
+class Apitome::DocsController < Object.const_get(Apitome.configuration.parent_controller)
   layout Apitome.configuration.layout
 
   helper_method *[
